e8d205e5bdf06cb7d3087a335d03b312ec5e4fa1
timmy <kris>
2022-06-28 21:55:07 +0200

    .

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -93,7 +93,7 @@ public class DemoResource {
         location.addMatch(match);
         location2.addMatch(match2);
         player.addUser(user);
-        player.test(match);
+        //player.addMatch(match);////testing
 
         //persisting
         em.persist(userRole);
